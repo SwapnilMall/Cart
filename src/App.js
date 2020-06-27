3bbebf7a7c36a5de8c792972c7a105aa7aa53e57
Swapnil Mall <swapnilmall03@gmail.com>
2020-06-27 12:10:25 +0530

    added firebase data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import React from 'react';
+import './index.css';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
 
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			products: [
-				{
-					price: 99,
-					title: 'Watch',
-					qty: 3,
-					img:
-						'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-					id: 1
-				},
-				{
-					price: 999,
-					title: 'Mobile Phone',
-					qty: 2,
-					img:
-						'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-					id: 2
-				},
-				{
-					price: 999,
-					title: 'Laptop',
-					qty: 1,
-					img:
-						'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
-					id: 3
-				}
-			]
+			products: [],
+			loading: true
 		};
-		// this.increaseQuantity = this.increaseQuantity.bind(this);
-		// this.testing();
 	}
+
+	componentDidMount() {
+		firebase.firestore().collection('products').get().then((snapshot) => {
+			const products = snapshot.docs.map((doc) => {
+				const data = doc.data();
+				data['id'] = doc.id;
+				return data;
+			});
+			this.setState({ products: products, loading: false });
+		});
+	}
+
 	handleIncreaseQuantity = (product) => {
-		console.log('Heyy please inc the qty of ', product);
 		const { products } = this.state;
 		const index = products.indexOf(product);
 
@@ -47,34 +34,33 @@ class App extends React.Component {
 			products
 		});
 	};
+
 	handleDecreaseQuantity = (product) => {
-		console.log('Heyy please inc the qty of ', product);
 		const { products } = this.state;
 		const index = products.indexOf(product);
 
 		if (products[index].qty === 0) {
 			return;
 		}
-
 		products[index].qty -= 1;
 
 		this.setState({
 			products
 		});
 	};
+
 	handleDeleteProduct = (id) => {
 		const { products } = this.state;
 
-		const items = products.filter((item) => item.id !== id); // [{}]
+		const items = products.filter((product) => product.id !== id);
 
 		this.setState({
 			products: items
 		});
 	};
 
-	getCartCount = () => {
+	getcountOfCartItems = () => {
 		const { products } = this.state;
-
 		let count = 0;
 
 		products.forEach((product) => {
@@ -84,29 +70,33 @@ class App extends React.Component {
 		return count;
 	};
 
-	getCartTotal = () => {
+	getcartTotal = () => {
 		const { products } = this.state;
 		let cartTotal = 0;
 
 		products.map((product) => {
-			cartTotal = cartTotal + product.qty * product.price;
+			if (product.qty > 0) {
+				cartTotal = cartTotal + product.qty * product.price;
+			}
+			return '';
 		});
 
 		return cartTotal;
 	};
 
 	render() {
-		const { products } = this.state;
+		const { products, loading } = this.state;
 		return (
 			<div className="App">
-				<Navbar count={this.getCartCount()} />
+				<Navbar count={this.getcountOfCartItems()} />
 				<Cart
-					products={products}
 					onIncreaseQuantity={this.handleIncreaseQuantity}
 					onDecreaseQuantity={this.handleDecreaseQuantity}
 					onDeleteProduct={this.handleDeleteProduct}
+					products={products}
 				/>
-				<div style={{ fontSize: 20, padding: 10 }}>TOTAL: {this.getCartTotal()}</div>
+				{loading && <h1>Loading Products...</h1>}
+				<div style={{ padding: 10, fontSize: 20 }}>TOTAL : {this.getcartTotal()}</div>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCSCsBSFRv8HnoDFeRg7WtpCv7gBvjreW4',
+	authDomain: 'cart-274e5.firebaseapp.com',
+	databaseURL: 'https://cart-274e5.firebaseio.com',
+	projectId: 'cart-274e5',
+	storageBucket: 'cart-274e5.appspot.com',
+	messagingSenderId: '246710121263',
+	appId: '1:246710121263:web:a53e643f38535f9048e7da'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
 	<React.StrictMode>
